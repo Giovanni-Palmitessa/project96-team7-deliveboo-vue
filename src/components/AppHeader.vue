7b1e468859e610487fc828965c615d85ab3d32c4
DavideFarci <yank92.df@gmail.com>
2023-08-23 15:48:14 +0200

    fix hover button login

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -89,7 +89,7 @@ export default {};
           <li>
             <button
               type="button"
-              class="text-white bg-secondary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
+              class="text-white bg-secondary hover:bg-b_hover focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
             >
               Login
             </button>
