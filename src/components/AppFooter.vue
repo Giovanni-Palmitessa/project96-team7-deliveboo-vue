ee90643bb0b4d06c00041d8685ec224436116f58
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-08-30 12:54:18 +0200

    add style in App404

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,11 @@
 <script></script>
 <template>
-  <div
-    class="text-white MyFooteer grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"
-  >
+  <div class="text-white MyFooteer grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
     <div class="cardFooter">
       <h1 class="mb-7 mt-4 font-bold text-xl">Scopri Deliveboo</h1>
       <ul>
         <li><a href="#">Investitori</a></li>
-        <li><a href="#">Chi siamo</a></li>
+        <li><router-link :to="{ name: 'about' }">Chi siamo</router-link></li>
         <li><a href="#">Ristoranti</a></li>
         <li><a href="#">Altro</a></li>
         <li><a href="#">Pressroom</a></li>
@@ -41,15 +39,9 @@
     </div>
   </div>
   <div class="icon text-white flex items-center gap-4 px-[50px] pb-4">
-    <a href="#"
-      ><i class="fa-brands fa-facebook text-2xl hover:text-b_hover"></i
-    ></a>
-    <a href="#"
-      ><i class="fa-brands fa-twitter text-2xl hover:text-b_hover"></i
-    ></a>
-    <a href="#"
-      ><i class="fa-brands fa-instagram text-2xl hover:text-b_hover"></i
-    ></a>
+    <a href="#"><i class="fa-brands fa-facebook text-2xl hover:text-b_hover"></i></a>
+    <a href="#"><i class="fa-brands fa-twitter text-2xl hover:text-b_hover"></i></a>
+    <a href="#"><i class="fa-brands fa-instagram text-2xl hover:text-b_hover"></i></a>
   </div>
 </template>
 <style scoped>
@@ -57,23 +49,28 @@
   background-color: #2e3333;
   height: 50px;
 }
+
 .MyFooteer {
   background-color: #2e3333;
   padding: 50px;
   width: 100%;
 }
+
 .cardFooter {
   background-color: #434848;
   border-radius: 0.5rem;
   padding-left: 15px;
   padding-bottom: 20px;
 }
+
 li {
   margin-top: 0.8rem;
 }
+
 li:hover {
   color: #00846b;
 }
+
 .appStoreApple {
   background-image: url(public/img/download-on-the-app-store-apple-logo-svgrepo-com.svg);
   background-size: cover;
@@ -82,6 +79,7 @@ li:hover {
   /* width: 65%; */
   background-position: center;
 }
+
 .appStoreGoogle {
   background-image: url(public/img/google-play-badge-logo-svgrepo-com.svg);
   background-size: cover;
