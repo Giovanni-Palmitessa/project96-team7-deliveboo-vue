1a248d51a18b84421605c49cf22d1532018a57c8
DavideFarci <yank92.df@gmail.com>
2023-08-30 13:00:09 +0200

    Create card restaurant

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,12 +1,14 @@
 <script>
-import AppFoodList from "../components/AppFoodList.vue";
+import AppRestaurant from "../components/AppRestaurant.vue";
 
-export default { components: { AppFoodList } };
+export default { components: { AppRestaurant } };
 </script>
 
 <template>
   <h1 class="mt-32">Resturant list</h1>
-  <AppFoodList />
+  <div class="mt-20 p-8 grid grid-cols-3 gap-4">
+    <AppRestaurant v-for="index in 6" :key="index" />
+  </div>
 </template>
 
 <style></style>
