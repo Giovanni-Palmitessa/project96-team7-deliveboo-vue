091e128540ef9feaa860a8edc3aec875195fab0a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-08-30 14:56:11 +0200

    add section for images

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -7,6 +7,74 @@ export default {};
     <h1 class="mt-28 text-3xl font-bold text-secondary">
       DeliveBoo: Gusta i Sapori del Mondo Comodamente a Casa Tua!
     </h1>
+
+    <p class="pt-5 text-xl text-gray-900">
+      Sei stanco di cucinare dopo una lunga giornata di lavoro? Vuoi gustare
+      prelibatezze culinarie senza alzarti dal divano? Benvenuto su DeliveBoo,
+      il luogo dove i tuoi desideri culinari diventano realtà con un semplice
+      click.
+    </p>
+
+    <div class="flex gap-5 mt-10">
+      <div class="image basis-full">
+        <img
+          class="w-9/12 py-10"
+          src="../../public/img/food-delivery.jpg"
+          alt="Delivery"
+        />
+      </div>
+
+      <div class="flex flex-col basis-9/12 pt-6">
+        <h2 class="text-2xl text-gray-900 font-bold">Cosa Offriamo:</h2>
+
+        <ul>
+          <li class="pt-6 text-xl">
+            <strong>Selezione Culinaria:</strong> Esplora un ampio assortimento
+            di cucine provenienti da tutto il mondo. Dalla pizza italiana alla
+            cucina thailandese, dalle prelibatezze giapponesi ai sapori
+            tradizionali locali, abbiamo tutto ciò di cui hai bisogno per
+            soddisfare i tuoi gusti.
+          </li>
+
+          <li class="pt-6 text-xl">
+            <strong>Facile Navigazione:</strong> Il nostro sito è intuitivo e
+            user-friendly. Scegli tra categorie come pizza, sushi, cibo
+            tradizionale, cucina etnica e molto altro. Trovare il tuo pasto
+            preferito non è mai stato così semplice.
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div></div>
+
+    <div class="flex gap-5 mt-14">
+      <div class="flex flex-col basis-9/12 pt-6">
+        <ul>
+          <li class="text-xl">
+            <strong>Personalizzazione:</strong> Hai preferenze alimentari
+            specifiche o allergie? Nessun problema. Nel nostro sistema di
+            ordinazione, puoi facilmente indicare le tue esigenze e
+            personalizzare ogni piatto secondo i tuoi gusti.
+          </li>
+
+          <li class="pt-6 text-xl">
+            <strong>Ordinazione Veloce:</strong> Basta perdere tempo al
+            telefono. Con pochi clic, puoi completare il tuo ordine e attendere
+            comodamente che il tuo pasto venga consegnato direttamente alla tua
+            porta.
+          </li>
+        </ul>
+      </div>
+
+      <div class="image basis-9/12">
+        <img
+          src="../../public/img/food-delivery-hotel1.jpg"
+          alt="food delivery"
+          class="rounded-xl"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
