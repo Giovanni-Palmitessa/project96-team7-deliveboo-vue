5c3888db9b7f6f164f06197513a050b3dc9a7c06
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-09-01 17:19:27 +0200

    import store

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,10 +1,18 @@
 <script>
-export default {};
+import { store } from "../store";
+
+export default {
+  methods: {
+    sendMailtoGuest() {
+      axios.post(this.store.baseUrl + "api/guests");
+    },
+  },
+};
 </script>
 <template>
   <h1>Riepilogo Ordine</h1>
 
-  <form class="my-32">
+  <form class="my-32" @submit.prevent="sendMailtoGuest" novalidate>
     <div class="relative z-0 w-full mb-6 group">
       <input
         type="email"
@@ -69,15 +77,15 @@ export default {};
       <div class="relative z-0 w-full mb-6 group">
         <input
           type="text"
-          name="company"
-          id="company"
+          name="message"
+          id="message"
           class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
         />
         <label
-          for="company"
+          for="message"
           class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >Compagnia</label
+          >Messaggio</label
         >
       </div>
     </div>
