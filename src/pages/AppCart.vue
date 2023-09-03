65654d45aad10182f7dc5700dc6b80b01b54c108
DavideFarci <yank92.df@gmail.com>
2023-09-03 04:07:18 +0200

    add modal to clear cart

--- src/pages/AppCart.vue
+++ src/pages/AppCart.vue
@@ -1,4 +1,5 @@
 <script>
+import { initFlowbite } from "flowbite";
 export default {
   data() {
     return {
@@ -60,6 +61,9 @@ export default {
   created() {
     this.getProductsCart();
     this.getSubtotal();
+  },
+  mounted() {
+    initFlowbite();
   },
 };
 </script>
@@ -215,11 +219,84 @@ export default {
             CHECKOUT
           </button>
           <button
-            class="transition-colors text-sm bg-white border border-gray-600 p-2 rounded-sm w-full text-gray-700 text-hover shadow-md"
-            @click="restoreCart"
+            class="block transition-colors text-sm bg-white border border-gray-600 p-2 rounded-sm w-full text-gray-700 text-hover shadow-md"
+            data-modal-target="popup-modal"
+            data-modal-toggle="popup-modal"
+            type="button"
           >
             SVUOTA CARRELLO
           </button>
+
+          <!-- modal  -->
+          <div
+            id="popup-modal"
+            tabindex="-1"
+            class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+          >
+            <div class="relative w-full max-w-md max-h-full shadow-lg">
+              <div
+                class="relative bg-b_hover rounded-lg shadow border-2 border-primary"
+              >
+                <button
+                  type="button"
+                  class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+                  data-modal-hide="popup-modal"
+                >
+                  <svg
+                    class="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
+                <div class="p-6 text-center">
+                  <svg
+                    class="mx-auto mb-4 text-gray-400 w-12 h-12"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="#FFC244"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                  <h3 class="mb-5 text-lg font-normal text-primary">
+                    Sei sicuro di voler svuotare il carrello?
+                  </h3>
+                  <button
+                    @click="restoreCart"
+                    data-modal-hide="popup-modal"
+                    type="button"
+                    class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                  >
+                    Sono sicuro!
+                  </button>
+                  <button
+                    data-modal-hide="popup-modal"
+                    type="button"
+                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+                  >
+                    No, cancella
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
