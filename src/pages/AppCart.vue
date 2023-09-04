12bcc71c8c3960a01a40607b8059490689b3c9b1
DavideFarci <yank92.df@gmail.com>
2023-09-04 13:07:08 +0200

    add function to remove an element from cart

--- src/pages/AppCart.vue
+++ src/pages/AppCart.vue
@@ -53,6 +53,13 @@ export default {
         this.products[index].qnt = 0;
       }
     },
+    removeCartEle(index) {
+      this.products.splice(index, 1);
+      this.updateCart();
+      this.getProductsCart();
+      this.getSubtotal();
+      console.log(localStorage);
+    },
     // Aggiorna il carrello nel localStorage
     updateCart() {
       localStorage.setItem("cart", JSON.stringify(this.products));
@@ -79,6 +86,7 @@ export default {
       <div
         class="flex flex-col p-4 text-lg font-semibold bg-primary shadow-md border rounded-lg"
         v-for="(product, index) in products"
+        :key="product.id"
       >
         <div class="flex flex-col md:flex-row gap-3 justify-between">
           <!-- Product Information -->
@@ -113,30 +121,8 @@ export default {
           </div>
           <!-- Remove Product Icon -->
           <div class="self-center">
-            <button class="">
-              <svg
-                class=""
-                height="24px"
-                width="24px"
-                id="Layer_1"
-                style="enable-background: new 0 0 512 512"
-                version="1.1"
-                viewBox="0 0 512 512"
-                xml:space="preserve"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-              >
-                <g>
-                  <path
-                    d="M400,113.3h-80v-20c0-16.2-13.1-29.3-29.3-29.3h-69.5C205.1,64,192,77.1,192,93.3v20h-80V128h21.1l23.6,290.7   c0,16.2,13.1,29.3,29.3,29.3h141c16.2,0,29.3-13.1,29.3-29.3L379.6,128H400V113.3z M206.6,93.3c0-8.1,6.6-14.7,14.6-14.7h69.5   c8.1,0,14.6,6.6,14.6,14.7v20h-98.7V93.3z M341.6,417.9l0,0.4v0.4c0,8.1-6.6,14.7-14.6,14.7H186c-8.1,0-14.6-6.6-14.6-14.7v-0.4   l0-0.4L147.7,128h217.2L341.6,417.9z"
-                  />
-                  <g>
-                    <rect height="241" width="14" x="249" y="160" />
-                    <polygon points="320,160 305.4,160 294.7,401 309.3,401" />
-                    <polygon points="206.5,160 192,160 202.7,401 217.3,401" />
-                  </g>
-                </g>
-              </svg>
+            <button @click="removeCartEle(index)">
+              <i class="fa-solid fa-trash text-secondary"></i>
             </button>
           </div>
         </div>
