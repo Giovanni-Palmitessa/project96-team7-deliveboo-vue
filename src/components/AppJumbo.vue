6a213f789fa9ed27f5a8e3888ec48a02ae24b6f3
DavideFarci <yank92.df@gmail.com>
2023-09-05 22:33:56 +0200

    fix negative margin in img wave (responsive)

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -32,7 +32,7 @@ export default {};
       </div>
     </div>
   </div>
-  <img src="../../public/img/wave.svg" alt="wave" class="mb-[-4rem]" />
+  <img src="../../public/img/wave.svg" alt="wave" class="md:mb-[-4rem]" />
 </template>
 
 <style scoped></style>
