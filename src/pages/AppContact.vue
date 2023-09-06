d51836ff2165e3e5de12dc8786ac8aba6feb9e75
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-09-06 18:34:31 +0200

    adjust style title

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -13,7 +13,6 @@ export default {
       email: "",
       name: "",
       surname: "",
-      phone: "",
       message: "",
       showSuccess: false,
       isSending: false,
@@ -34,7 +33,6 @@ export default {
           email: this.email,
           name: this.name,
           surname: this.surname,
-          phone: this.phone,
           message: this.message,
         })
         .then((response) => {
@@ -58,7 +56,6 @@ export default {
       this.email = "";
       this.name = "";
       this.surname = "";
-      // this.phone = "";
       this.message = "";
     },
     getProductsCart() {
@@ -267,10 +264,10 @@ export default {
       </button>
     </div>
 
-    <h1>Riepilogo Ordine</h1>
+    <h1 class="text-5xl text-center font-bold">Riepilogo Ordine</h1>
 
     <form
-      class="my-32 max-w-md mx-auto"
+      class="my-32 max-w-7xl mx-auto"
       @submit.prevent="payWithCreditCard"
       novalidate
     >
@@ -280,7 +277,6 @@ export default {
           type="email"
           id="email"
           class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder=" "
           required
         />
         <label
@@ -296,7 +292,6 @@ export default {
             type="text"
             id="name"
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none -blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
           />
           <label
             for="name"
@@ -310,7 +305,6 @@ export default {
             type="text"
             id="surname"
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none -blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
           />
           <label
             for="surname"
@@ -320,21 +314,6 @@ export default {
         </div>
       </div>
       <div class="grid md:grid-cols-2 md:gap-6">
-        <!-- <div class="relative z-0 w-full mb-6 group">
-          <input
-            v-model="phone"
-            type="tel"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            id="phone"
-            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-          />
-          <label
-            for="phone"
-            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >Numero di telefono</label
-          >
-        </div> -->
         <div class="relative z-0 w-full mb-6 group">
           <input
             v-model="message"
@@ -342,7 +321,6 @@ export default {
             name="message"
             id="message"
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
           />
           <label
             for="message"
@@ -351,30 +329,7 @@ export default {
           >
         </div>
       </div>
-      <!-- <button
-        type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-        :disabled="isSending"
-      >
-        Submit
-      </button> -->
 
-      <!-- <div class="form-group">
-        <label for="amount">Amount</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text">$</span>
-          </div>
-          <input
-            type="number"
-            id="amount"
-            v-model="amount"
-            class="form-control"
-            placeholder="Enter Amount"
-          />
-        </div>
-      </div> -->
-      <hr />
       <div class="form-group">
         <label>Credit Card Number</label>
         <div id="creditCardNumber" class="form-control"></div>
@@ -398,7 +353,7 @@ export default {
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         :disabled="isSending"
       >
-        Submit
+        Paga Ora!
       </button>
     </form>
 
