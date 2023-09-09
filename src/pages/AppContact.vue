7980552a8a71926e921e2a541d0cc2b2b9f383b2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-09-09 11:44:10 +0200

    fix style name for payment form

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -273,29 +273,59 @@ export default {
             >Email address</label
           >
         </div>
-
         <div v-if="emailError" class="text-red-500 mt-2">
           {{ emailError }}
         </div>
 
+        <!-- NOME E COGNOME -->
+
         <div class="grid md:grid-cols-2 md:gap-6">
           <div class="relative z-0 w-full mb-6 group">
+            <input
+              v-model="name"
+              type="text"
+              name="floating_first_name"
+              id="floating_first_name"
+              class="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer"
+              autocomplete="off"
+              required
+            />
             <label
               for="name"
+              class="peer-focus:font-medium absolute text-base text-secondary duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >Nome</label
+            >
+          </div>
+          <div v-if="nameError" class="text-red-500 mt-2">
+            {{ nameError }}
+          </div>
+
+          <div class="relative z-0 w-full mb-6 group">
+            <input
+              type="text"
+              id="name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_last_name"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >Last name</label
+            >
+          </div>
+        </div>
+
+        <div class="grid md:grid-cols-2 md:gap-6">
+          <!-- <div class="relative z-0 w-full mb-6 group">
+            <label
               class="peer-focus:font-medium absolute text-lg text-secondary duration-300 transform -translate-y-6 scale-75 bottom-4 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 mb-2"
               >Nome</label
             >
             <input
-              v-model="name"
-              type="text"
-              id="name"
               class="block py-4 px-0 w-full text-lg text-gray-900 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              autocomplete="off"
             />
-            <div v-if="nameError" class="text-red-500 mt-2">
-              {{ nameError }}
-            </div>
-          </div>
+          </div> -->
 
           <div class="relative z-0 w-full mb-6 group">
             <label
