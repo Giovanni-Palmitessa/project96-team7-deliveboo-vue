3ddfe276f8e33faa24d66802bba2d6876f9eaf01
DavideFarci <yank92.df@gmail.com>
2023-09-12 00:09:50 +0200

    fix page thankyou

--- src/pages/AppThankYou.vue
+++ src/pages/AppThankYou.vue
@@ -4,12 +4,19 @@ export default {};
 </script>
 
 <template>
-  <div class="bg-primary py-[83px]">
+  <div class="bg-primary_hover py-[83px]">
     <div class="container mx-auto pt-32 p-2">
-      <div class="flex flex-col">
-        <span class="text-4xl font-bold text-secondary text-center">GRAZIE PER AVER ACQUISTATO DA DELIVEBOO</span>
-        <RouterLink :to="{ name: 'home' }"
-          class="mx-auto mt-12 p-4 text-md text-primary font-semibold bg-secondary hover:bg-b_hover rounded-md shadow-md">
+      <div class="flex flex-col py-8 bg-primary rounded-lg shadow-md">
+        <span class="text-4xl font-bold text-secondary text-center"
+          >GRAZIE PER AVER ACQUISTATO DA DELIVEBOO</span
+        >
+        <p class="text-center mt-3 text-b_hover">
+          Riceverai una mail con la conferma di avvenuto ordine
+        </p>
+        <RouterLink
+          :to="{ name: 'home' }"
+          class="mx-auto mt-12 p-4 text-md text-primary font-semibold bg-secondary hover:bg-b_hover rounded-md shadow-md"
+        >
           Continua con gli acquisti
         </RouterLink>
       </div>
