f31f0a5795a0e63025df37085fd6f9faef02db73
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-09-11 13:13:53 +0200

    fix responsive

--- src/pages/AppThankYou.vue
+++ src/pages/AppThankYou.vue
@@ -4,16 +4,16 @@ export default {};
 </script>
 
 <template>
-  <div class="grid grid-cols-1 p-60 text-center bg-primary">
-    <span class="text-4xl font-bold text-secondary"
-      >GRAZIE PER AVER ACQUISTATO DA DELIVEBOO</span
-    >
-    <RouterLink
-      :to="{ name: 'home' }"
-      class="mx-auto mt-12 p-4 text-md text-primary font-semibold bg-secondary hover:bg-b_hover rounded-md shadow-md"
-    >
-      Continua con gli acquisti
-    </RouterLink>
+  <div class="bg-primary py-[83px]">
+    <div class="container mx-auto pt-32 p-2">
+      <div class="flex flex-col">
+        <span class="text-4xl font-bold text-secondary text-center">GRAZIE PER AVER ACQUISTATO DA DELIVEBOO</span>
+        <RouterLink :to="{ name: 'home' }"
+          class="mx-auto mt-12 p-4 text-md text-primary font-semibold bg-secondary hover:bg-b_hover rounded-md shadow-md">
+          Continua con gli acquisti
+        </RouterLink>
+      </div>
+    </div>
   </div>
 </template>
 
