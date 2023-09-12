860874a0ea79e9b508e8ea134cf20de9691d8ef6
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-09-12 09:07:28 +0200

    removed useless tag a

--- src/components/AppRestaurant.vue
+++ src/components/AppRestaurant.vue
@@ -28,27 +28,23 @@ export default {
   <div
     class="bg-primary border border-gray-200 rounded-lg shadow flex flex-col"
   >
-    <a href="#">
-      <img
-        class="rounded-t-lg h-[250px] min-w-full object-cover"
-        :src="getImageUrl(restaurant.url_image)"
-        :alt="restaurant.name"
-      />
-      <!-- <img class="rounded-t-lg" src="../../public/img/rest01.jpg" alt="" /> -->
-    </a>
+    <img
+      class="rounded-t-lg h-[250px] min-w-full object-cover"
+      :src="getImageUrl(restaurant.url_image)"
+      :alt="restaurant.name"
+    />
+    <!-- <img class="rounded-t-lg" src="../../public/img/rest01.jpg" alt="" /> -->
     <div class="p-5 bg-primary">
-      <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-          {{ restaurant.name }}
-        </h5>
-      </a>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        {{ restaurant.name }}
+      </h5>
       <p class="mb-3 font-normal text-sm text-gray-700">
         {{ restaurant.description }}
       </p>
       <template v-for="category in restaurant.categories">
-        <a class="flex gap-2 mb-3 text-sm font-semibold" href="#">
+        <p class="flex gap-2 mb-3 text-sm font-semibold">
           {{ category.name }}
-        </a>
+        </p>
       </template>
       <RouterLink
         :to="{ name: 'menu' }"
